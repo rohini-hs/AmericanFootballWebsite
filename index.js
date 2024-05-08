241fa63a423305be6ff72cc97c1492a2d9ff4b62
rohini-hs <167188786+rohini-hs@users.noreply.github.com>
2024-05-08 16:28:02 +0530

    created dropdown list

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+  window.onload = function() 
+  {
+
+    // load the dropdown list (see function below)
+    initTeamsDropdown();
+}
+    // other stuff
+
+    function initTeamsDropdown() {
+
+        let teams = [
+            { teamcode:"DAL", 
+              name:"Dallas Cowboys", 
+              locatedIn:"Arlington, TX"
+            },
+            { teamcode:"DEN", 
+              name:"Denver Broncos", 
+              locatedIn:"Denver, CO"
+            },
+            { teamcode:"HOU", 
+              name:"Houston Texans", 
+              locatedIn:"Houston, TX"
+            },
+            { teamcode:"KAN", 
+              name:"Kansas City Chiefs", 
+              locatedIn:"Kansas City, MO"
+            }
+          ];
+
+
+        // load the dropdown list
+    
+        const TeamsList = document.getElementById("football");
+     
+        let length = teams.length;
+        for (let i = 0; i < length; i++) {
+     
+           // create the option element and set the text and
+           // value at the same time
+           let theOption = new Option(teams[i].name, teams[i].teamcode); 
+     
+           // append the option as a child of (inside) the 
+           // select element
+           TeamsList.appendChild(theOption);
+        }
+     }
+     
+     
+     
+ 
+ 
